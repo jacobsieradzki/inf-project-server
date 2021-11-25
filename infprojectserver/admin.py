13d9af173b22c37d3ad2da384038d48380212e67
Jake Ryan <jake@springnation.co.uk>
2021-11-25 23:25:34 +0000

    Initial commit

---
+++ infprojectserver/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from infprojectserver.models import Course, Organisation
+
+# admin.site.register(Organisation)
+# admin.site.register(Course)
---
+++ infprojectserver/models/Organisation.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Organisation(models.Model):
+    id = models.CharField(max_length=20, primary_key=True)
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
