06b748c71b6ab47fef64c45801c0f93979183680
Jake Ryan <jake@springnation.co.uk>
2021-12-07 17:45:28 +0000

    Settings

--- classroom/settings.py
+++ classroom/settings.py
@@ -84,9 +84,9 @@ WSGI_APPLICATION = 'classroom.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE" : "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
     }
 }
 
