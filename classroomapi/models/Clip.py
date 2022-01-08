edc253915de57a7fe095f2605d3a609ed84d3f0c
Jake Ryan <jake@springnation.co.uk>
2022-01-08 17:30:20 +0000

    Fixed default error in Clip model

--- classroomapi/models/Clip.py
+++ classroomapi/models/Clip.py
@@ -9,12 +9,13 @@ class Clip(models.Model):
         VIDEO_CLIP = 'VIDEO_CLIP', _('Video Clip')
         PDF_PAGE = 'PDF_PAGE', _('PDF Page')
         PDF_CLIP = 'PDF_CLIP', _('PDF Clip')
+        NONE = 'NONE', _('None')
 
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
     content = models.TextField()
     description = models.CharField(max_length=60, blank=True)
-    type = models.CharField(max_length=16, choices=ClipType.choices, default=ClipType.LECTURE)
+    type = models.CharField(max_length=16, choices=ClipType.choices, default=ClipType.NONE)
     start_location = models.IntegerField()
     end_location = models.IntegerField()
 
