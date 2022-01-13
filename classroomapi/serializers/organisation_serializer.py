9ecebfa705970cc8e2f42858397012695c63fc58
Jake Ryan <jake@springnation.co.uk>
2022-01-13 12:15:28 +0000

    Added image url to Organisation serializer

--- classroomapi/serializers/organisation_serializer.py
+++ classroomapi/serializers/organisation_serializer.py
@@ -5,4 +5,4 @@ from classroomapi.models import Organisation
 class OrganisationSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Organisation
-        fields = ['id', 'name']
+        fields = ['id', 'name', 'image_url']
